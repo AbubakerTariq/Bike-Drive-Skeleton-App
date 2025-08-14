1de96b3d9ecf42d6510dc66c2307daa512427cc7
GabrielAguirreOllinger <95169259+GabrielAguirreOllinger@users.noreply.github.com>
2025-08-14 14:08:09 +0800

    RHB firmware still not receiving the Set Target messages from the game, but...
    fixed problem with the data logging:
    Firmware was sending data feedback at a rate of 5 msec.
    The game's timer was checking for feedback messages at a rate of 1 msec.

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -3,18 +3,46 @@ using System.IO;
 using System.Timers;
 using System.Threading;
 using UnityEngine;
+using Articares.Distal;
 
 public class DataManager : MonoBehaviour
 {
     private static DataManager instance;
-    
+
+    /////////////////////////////////////////////////////////////////////////
+    // Data storage vars:
+    /////////////////////////////////////////////////////////////////////////
+
+    public string FILE_EXT = ".csv";
+
+    public string DATA_FILE_DIR = "_data_rhb_unity_bike/";
+    public string DATA_FILENAME_DEF = "data_rhb_bike_";
+
     // Variable to store file path of the data file
-    private string filePath;
-    
-    // The thread and timer to help save the data in the CSV file
+    private string dataFilePath;
+
+    /////////////////////////////////////////////////////////////////////////
+    // Time step vars:
+    /////////////////////////////////////////////////////////////////////////   
+
+    private const int DECIM_DATA_COUNT = 1;
+    private int data_count = 0;
+
+    private float t_step_prev = 0f;
+    private float t_step_ref;
+
+    // Thread and timer to help save the data in the data file:
+    const double DATA_TIMER_INTERVAL_MSEC = 50f;
+
     private System.Timers.Timer dataTimer;
     private Thread timerThread;
 
+    private readonly object fileLock = new object();
+
+    /////////////////////////////////////////////////////////////////////////
+    // Methods:
+    /////////////////////////////////////////////////////////////////////////
+    ///
     private void Awake()
     {
         // Singleton logic
@@ -36,17 +64,22 @@ public class DataManager : MonoBehaviour
 
     private void SetupDataFile()
     {
-        // The first parameter of the Path.Combine function takes in the directory and the second takes in the File name.
-        // Set up the path name according to your needs.
-        filePath = Path.Combine(Application.persistentDataPath, "data.csv");
+        string dataFileName = DATA_FILENAME_DEF + DateTimeStamp() + FILE_EXT;
+        dataFilePath = DATA_FILE_DIR + dataFileName;
 
-        // The headings to be set up in the CSV file
-        string[] headers = new[] { "Date Time", "Uptime", "PositionR", "VelocityR", "PositionP", "VelocityP", "Cartesian Position", "Direction Angle", "Speed", "Tilt Angle" };
+        // The headings to be set up in the data file
+        // Removed "Date Time" (13.08.2025)
+        string[] headers = new[] { "t sec", "dt sec", "pos radial", "vel radial", "pos rot", "vel rot", 
+            "pos x", "pos z", "angle dir", "vel magn", "angle tilt" };
 
-        if (!File.Exists(filePath))
+        if (!File.Exists(dataFilePath))
         {
             string headerLine = string.Join(",", headers);
-            File.WriteAllText(filePath, $"{headerLine}\n");
+            File.WriteAllText(dataFilePath, $"{headerLine}\n");
+
+            // Display section:
+            ExternalConsoleLogger.Log("____________________________________________________________________");
+            ExternalConsoleLogger.Log("SetupDataFile(): created file [" + dataFileName + "]\n");
         }
     }
 
@@ -78,8 +111,9 @@ public class DataManager : MonoBehaviour
 
         timerThread = new Thread(() =>
         {
-            dataTimer = new(1f);
-            dataTimer.Elapsed += OnTimerElapsed;
+            // dataTimer = new(1f);
+            dataTimer = new System.Timers.Timer(DATA_TIMER_INTERVAL_MSEC);
+            dataTimer.Elapsed += SaveDataOnTimerElapsed;
             dataTimer.AutoReset = true;
             dataTimer.Start();
         });
@@ -93,52 +127,92 @@ public class DataManager : MonoBehaviour
         dataTimer?.Dispose();
     }
 
-    private int counter = 0;
-
-    private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+    private void SaveDataOnTimerElapsed(object sender, ElapsedEventArgs e)
     {
-        if (counter > 0)
+        // Modified counter (13.08.2025):
+        if (data_count % DECIM_DATA_COUNT == 0)
         {
-            if (counter >= 5)
-            {
-                counter = 0;
-            }
-            else
-            {
-                counter++;
-                return;
-            }
+            DistalComm.ExerciseData DistalData = ReHandyBotController.Instance.DistalData;
+            SaveDataEntry(DistalData, 0f, 0f, 0f, 0f, 0f);
         }
 
-        // Passing 0 as dummy values for Cartesian Position, Direction Angle, Speed, and Tilt Angle
-        SaveDataEntry(0f, 0f, 0f, 0f);
-        counter++;
+        data_count++;
     }
 
-    private readonly object fileLock = new object();
-    private void SaveDataEntry(float cartesianPosition, float directionAngle, float speed, float tiltAngle)
+    private void SaveDataEntry( DistalComm.ExerciseData DistalData, 
+        float cartesianPositionX, float cartesianPositionZ, float directionAngle, float speed, float tiltAngle)
     {
+        // string datetime = DateTime.UtcNow.ToLocalTime().ToString("MMM-dd-yyyy HH:mm:ss.fff tt \"GMT\"zzz");
+
+        /////////////////////////////////////////////////////////////////////////
+        // Compute time step:
+        /////////////////////////////////////////////////////////////////////////
+        ///
+        const float MSEC_PER_SEC = 1000f;
+       
+        float t_step;
+        float dt_step;
+
+        if (data_count == 0)
+        {
+            t_step_ref = DistalData.UptimeMs / MSEC_PER_SEC;
+            t_step = 0f;
+            dt_step = 0f;
+        }
+        else
+        {
+            t_step = DistalData.UptimeMs / MSEC_PER_SEC - t_step_ref;
+            dt_step = t_step - t_step_prev;
+        }
+
+        string t_step_str = t_step.ToString("F3");
+        string dt_step_str = dt_step.ToString("F3");
+
+        /////////////////////////////////////////////////////////////////////////
+        // Save data step to file:
+        /////////////////////////////////////////////////////////////////////////
+
         try
         {
-            string datetime = DateTime.UtcNow.ToLocalTime().ToString("MMM-dd-yyyy HH:mm:ss.fff tt \"GMT\"zzz") ;
-            string output = $"{datetime}," +
-                            $"{ReHandyBotController.Instance.DistalData.UptimeMs}," +
-                            $"{ReHandyBotController.Instance.DistalData.PositionR}," +
-                            $"{ReHandyBotController.Instance.DistalData.VelocityR}," +
-                            $"{ReHandyBotController.Instance.DistalData.PositionP}," +
-                            $"{ReHandyBotController.Instance.DistalData.VelocityP}," +
-                            $"{cartesianPosition}," +
-                            $"{directionAngle}," +
-                            $"{speed}," +
-                            $"{tiltAngle}";
+            // "t sec", "dt sec", "pos radial", "vel radial", "pos rot", "vel rot", "pos x", "pos z", "angle dir", "vel magn", "angle tilt" };
+            string output = 
+                t_step_str + "," +
+                dt_step_str + "," +
+                $"{DistalData.PositionR}," +
+                $"{DistalData.VelocityR}," +
+                $"{DistalData.PositionP}," +
+                $"{DistalData.VelocityP}," +
+                $"{cartesianPositionX}," +
+                $"{cartesianPositionZ}," +
+                $"{directionAngle}," +
+                $"{speed}," +
+                $"{tiltAngle}";
             lock (fileLock)
             {
-                File.AppendAllText(filePath, $"{output}\n");
+                File.AppendAllText(dataFilePath, $"{output}\n");
             }
         }
         catch (Exception ex)
         {
             Debug.LogError($"Error saving data: {ex.Message}");
         }
+
+        /////////////////////////////////////////////////////////////////////////
+        // Save time step for next iteration:
+        /////////////////////////////////////////////////////////////////////////
+        
+        t_step_prev = t_step;
+    }
+
+    public string DateTimeStamp()
+    {
+        string year = DateTime.Now.Year.ToString("0000");
+        string month = DateTime.Now.Month.ToString("00");
+        string date = DateTime.Now.Day.ToString("00");
+        string hour = DateTime.Now.Hour.ToString("00");
+        string minute = DateTime.Now.Minute.ToString("00");
+        string second = DateTime.Now.Second.ToString("00");
+
+        return year + month + date + "_" + hour + minute + second;
     }
 }
